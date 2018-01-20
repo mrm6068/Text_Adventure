35647447e3e51900a36bf9f3605e87d58e1f445e
Mike Morrell <Mike080188@msn.com>
2018-01-20 15:02:57 -0500

    test

---
+++ items.py
@@ -0,0 +1,49 @@
+﻿# Base class for all items
+class Item():
+    # __init__ is the contructor method
+    def __init__(self, name, description, value):
+        self.name = name   # attribute of the Item class and any subclasses
+        self.description = description # attribute of the Item class and any subclasses
+        self.value = value # attribute of the Item class and any subclasses
+    
+    # __str__ method is used to print the object
+    def __str__(self):
+        return "{}\n=====\n{}\nValue: {}\n".format(self.name, self.description, self.value)
+
+# Extend the Items class
+# Gold class will be a child or subclass of the superclass Item
+class Gold(Item):
+    # __init__ is the contructor method
+    def __init__(self, amt): 
+        self.amt = amt # attribute of the Gold class
+        super().__init__(name="Gold",
+                         description="A round coin with {} stamped on the front.".format(str(self.amt)),
+                         value=self.amt)
+
+class Weapon(Item):
+    def __init__(self, name, description, value, damage):
+        self.damage = damage
+        super().__init__(name, description, value)
+ 
+    def __str__(self):
+        return "{}\n=====\n{}\nValue: {}\nDamage: {}".format(self.name, self.description, self.value, self.damage)
+ 
+class Rock(Weapon):
+    def __init__(self):
+        super().__init__(name="Rock",
+                         description="A fist-sized rock, suitable for bludgeoning.",
+                         value=0,
+                         damage=5)
+ 
+class Dagger(Weapon):
+    def __init__(self):
+        super().__init__(name="Dagger",
+                         description="A small dagger with some rust. Somewhat more dangerous than a rock.",
+                         value=10,
+                         damage=10)
+class Pillow(Weapon):
+    def __init__(self):
+        super().__init__(name="Pillow",
+                         description="A pillow for handling my soft work.",
+                         value=1,
+                         damage=1)
---
+++ tiles.py
@@ -0,0 +1,128 @@
+import items, enemies, actions, world
+ 
+class MapTile:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def intro_text(self):
+        raise NotImplementedError()
+ 
+    def modify_player(self, player):
+        raise NotImplementedError()
+
+    def adjacent_moves(self):
+        """Returns all move actions for adjacent tiles."""
+        moves = []
+        if world.tile_exists(self.x + 1, self.y):
+            moves.append(actions.MoveEast())
+        if world.tile_exists(self.x - 1, self.y):
+            moves.append(actions.MoveWest())
+        if world.tile_exists(self.x, self.y - 1):
+            moves.append(actions.MoveNorth())
+        if world.tile_exists(self.x, self.y + 1):
+            moves.append(actions.MoveSouth())
+        return moves
+ 
+    def available_actions(self):
+        """Returns all of the available actions in this room."""
+        moves = self.adjacent_moves()
+        moves.append(actions.ViewInventory())
+ 
+        return moves
+
+
+class StartingRoom(MapTile):
+    # override the intro_text method in the superclass
+    def intro_text(self):
+        return """
+        You find yourself in a cave with a flickering torch on the wall.
+        You can make out four paths, each equally as dark and foreboding.
+        """
+ 
+    def modify_player(self, player):
+        #Room has no action on player
+        pass
+
+class LootRoom(MapTile):
+    def __init__(self, x, y, item):
+        self.item = item
+        super().__init__(x, y)
+ 
+    def add_loot(self, player):
+        player.inventory.append(self.item)
+ 
+    def modify_player(self, player):
+        self.add_loot(player)
+
+class EnemyRoom(MapTile):
+    def __init__(self, x, y, enemy):
+        self.enemy = enemy
+        super().__init__(x, y)
+ 
+    def modify_player(self, the_player):
+        if self.enemy.is_alive():
+            the_player.hp = the_player.hp - self.enemy.damage
+            print("Enemy does {} damage. You have {} HP remaining.".format(self.enemy.damage, the_player.hp))
+
+class EmptyCavePath(MapTile):
+    def intro_text(self):
+        return """
+        Another unremarkable part of the cave. You must forge onwards.
+        """
+ 
+    def modify_player(self, player):
+        #Room has no action on player
+        pass
+ 
+class GiantSpiderRoom(EnemyRoom):
+    def __init__(self, x, y):
+        super().__init__(x, y, enemies.GiantSpider())
+ 
+    def intro_text(self):
+        if self.enemy.is_alive():
+            return """
+            A giant spider jumps down from its web in front of you!
+            """
+        else:
+            return """
+            The corpse of a dead spider rots on the ground.
+            """
+ 
+class FindDaggerRoom(LootRoom):
+    def __init__(self, x, y):
+        super().__init__(x, y, items.Dagger())
+ 
+    def intro_text(self):
+        return """
+        Your notice something shiny in the corner.
+        It's a dagger! You pick it up.
+        """
+
+class EnemyRoom(MapTile):
+    def __init__(self, x, y, enemy):
+        self.enemy = enemy
+        super().__init__(x, y)
+ 
+    def modify_player(self, the_player):
+        if self.enemy.is_alive():
+            the_player.hp = the_player.hp - self.enemy.damage
+            print("Enemy does {} damage. You have {} HP remaining.".format(self.enemy.damage, the_player.hp))
+ 
+    def available_actions(self):
+        if self.enemy.is_alive():
+            return [actions.Flee(tile=self), actions.Attack(enemy=self.enemy)]
+        else:
+            return self.adjacent_moves()
+
+class LeaveCaveRoom(MapTile):
+    def intro_text(self):
+        return """
+        You see a bright light in the distance...
+        ... it grows as you get closer! It's sunlight!
+ 
+ 
+        Victory is yours!
+        """
+ 
+    def modify_player(self, player):
+        player.victory = True
